636d781bea300b9c9770360dc7f479ac29a0a7b4
kohrongying <kohrongying@gmail.com>
2020-03-30 23:27:16 +0800

    Add main structure and styles

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp'),
+  browsersync = require('browser-sync').create(),
+  minifyCSS = require('gulp-cssmin'),
+  minifyHTML = require('gulp-minify-html');
+
+function css() {
+  return gulp
+    .src('src/*.css') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist'))
+    .pipe(browsersync.stream());
+}
+
+function html() {
+  return gulp
+    .src('src/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist'))
+    .pipe(browsersync.stream());
+}
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./dist/"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function watchFiles() {
+  gulp.watch('src/*.css', css);
+  gulp.watch('src/*.html', html);
+}
+
+const build = gulp.series(html, css);
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
---
+++ src/README.md
@@ -0,0 +1,9 @@
+Development
+```
+gulp watch 
+```
+
+Build
+```
+gulp or gulp build
+```
