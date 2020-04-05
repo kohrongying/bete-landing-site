898f4b9da8e716b114ce097e76c1c0303db35432
kohrongying <kohrongying@gmail.com>
2020-04-05 18:32:45 +0800

    Add images and update gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
 const gulp = require('gulp'),
+  del = require('del'),
   browsersync = require('browser-sync').create(),
-  minifyCSS = require('gulp-cssmin'),
-  minifyHTML = require('gulp-minify-html');
+  cleanCSS  = require('gulp-clean-css'),
+  htmlmin = require('gulp-htmlmin');
 
 function css() {
   const postcss = require('gulp-postcss');
   return gulp
-    .src('src/*.css') // Gets all files ending with .scss in app/scss and children dirs
+    .src('src/*.css')
     .pipe(postcss([
-      // ...
       require('tailwindcss'),
-      minifyCSS
+      cleanCSS
     ]))
     .pipe(gulp.dest('docs'))
+    .pipe(browsersync.stream());
+}
+
+function images() {
+  return gulp 
+    .src('src/assets/*')
+    .pipe(gulp.dest('docs/assets'))
     .pipe(browsersync.stream());
 }
 
 function html() {
   return gulp
     .src('src/*.html')
-    .pipe(minifyHTML())
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('docs'))
     .pipe(browsersync.stream());
+}
+
+async function clean() {
+  return del(['docs', 'docs/assets'])
 }
 
 // BrowserSync
@@ -35,23 +46,19 @@ function browserSync(done) {
   done();
 }
 
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
 
 function watchFiles() {
   gulp.watch('src/*.css', css);
   gulp.watch('src/*.html', html);
 }
 
-const build = gulp.series(html, css);
-const watch = gulp.parallel(watchFiles, browserSync);
+const build = gulp.series(clean, images, html, css);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // export tasks
 exports.css = css;
 exports.html = html;
+exports.images = images;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
